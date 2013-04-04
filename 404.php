9ff5d737214a607fd6fce2cfa9a10cea41071390
Paulo Barcelos <paulo@azucrina.org>
2013-04-05 00:29:29 +0200

    Added comming soon

--- 404.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); ?>
-<div id="page" class="content">
-	<article>
-		<div class="pagecopy">
-			<h2>404 / Page not found</h2>
-		</div>			
-	</article>			
-</div>
-<?php get_footer(); ?>
